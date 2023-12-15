4eb683e797d60b0ca5b467361e517dfc212b04c5
Nitesh Mint <nitesh.raya058@gmail.com>
2023-12-15 12:58:45 +0545

    final

--- room/urls.py
+++ room/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path("rooms/", views.rooms, name="rooms"),
+    path("payment_completed/", views.payment_done, name="payment"),
 ]
--- room/views.py
+++ room/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.contrib.auth.decorators import login_required
 
+from .models import Room
 
 @login_required(login_url='login')
 def index(request):
@@ -9,3 +10,10 @@ def index(request):
         'user': request.user
     }
     return render(request, 'index.html', context)
+
+def rooms(request):
+    room = Room.objects.all()
+    return render(request, "room.html", context={"room":room})
+
+def payment_done(request):
+    return render(request, 'payment.html')
